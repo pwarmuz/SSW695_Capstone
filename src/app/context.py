0265f237b70f351f9a4fc371cc1d289aed1bbd67
enptfb55 <enptfb55@gmail.com>
2017-02-11 19:47:29 -0500

    finished authentication

---
+++ src/app/context.py
@@ -0,0 +1,16 @@
+from flask import g, current_app
+from pymongo import MongoClient
+
+
+def get_db():
+    """ Get current application mongo client
+
+    :returns New MongoClient if there is none in the current application context, otherwise existing MongoClient
+    """
+    if not hasattr(g, 'mongo_client'):
+        g.mongo_client = MongoClient(current_app.config.get("MONGO_HOST"), port=current_app.config.get("MONGO_PORT"))
+
+        g.mongo_client.admin.authenticate(current_app.config.get("MONGO_USER"),
+                                          current_app.config.get("MONGO_PASSWORD"),
+                                          mechanism=current_app.config.get("MONGO_AUTH_MECH"))
+    return g.mongo_client
--- src/run.py
+++ src/run.py
@@ -3,7 +3,6 @@
 """ Run Web Server """
 import sys
 import signal
-import config_public as config
 from app import flask_app
 
 """ This fixes the slow Ctrl+C python interpreter """
@@ -12,7 +11,6 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def local_server():
     """ Launch local server """
-    flask_app.config.from_object(config.BaseConfig)
     flask_app.run(host="127.0.0.1", port=5000, threaded=True, debug=True)
     sys.exit(1)
 
