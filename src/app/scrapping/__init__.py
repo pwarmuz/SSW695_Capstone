831249cc6e7949c134816b983e1041c641f16e23
Constantine Davantzis <c.davantzis@gmail.com>
2017-02-22 18:30:21 -0500

    Complete Parsing

--- src/app/scrapping/__init__.py
+++ src/app/scrapping/__init__.py
@@ -1,4 +1,1 @@
-
 import stevens_bookstore
-
-
--- src/app/scrapping/stevens_bookstore.py
+++ src/app/scrapping/stevens_bookstore.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python
+# coding=utf-8
 """ Module For Scraping Stevens Bookstore """
 import time
-import platform
 import pickle
-
 import requests
+import json
+
 from selenium import webdriver
 
+from webdrivers import get_geckodriver_path
 
 __author__ = "Constantine Davantzis"
 __status__ = "Development"
-
-WINDOWS32_GECKODRIVER_PATH = "webdrivers/geckodriver-v0.14.0-win32/geckodriver"
-WINDOWS64_GECKODRIVER_PATH = "webdrivers/geckodriver-v0.14.0-win64/geckodriver"
-LINUX32_GECKODRIVER_PATH = "webdrivers/geckodriver-v0.14.0-linux32/geckodriver"
-LINUX64_GECKODRIVER_PATH = "webdrivers/geckodriver-v0.14.0-linux64/geckodriver"
-
-
-def get_geckodriver_path():
-    system_name = platform.system()
-    is_64_bit = platform.machine().endswith('64')
-
-    if system_name == "Windows":
-        return WINDOWS64_GECKODRIVER_PATH if is_64_bit else WINDOWS32_GECKODRIVER_PATH
-    return LINUX64_GECKODRIVER_PATH if is_64_bit else LINUX32_GECKODRIVER_PATH
 
 
 def update_book_id_list(executable_path=get_geckodriver_path()):
@@ -65,16 +53,51 @@ def get_book_id_list():
     return pickle.load(open('book_id_list.p', 'rb'))
 
 
+def save_all_books_page():
+    r = requests.post('http://www.stevenscampusstore.com/textbook_express.asp?mode=2&step=2', data={'sectionIds': get_book_id_list()})
+    with open("all_books.html", "w") as text_file:
+        text_file.write(r.content)
 
-#r = requests.post('http://www.stevenscampusstore.com/textbook_express.asp?mode=2&step=2', data={'sectionIds': get_book_id_list()})
-#with open("all_books.html", "w") as text_file:
-#    text_file.write(r.content)
-#from bs4 import BeautifulSoup
 
-#html_doc = str(open("all_books.html", "r").readlines())
-#soup = BeautifulSoup(html_doc, 'html.parser')
-#books = soup.findAll("tr", {"class": "book"})
+def parse_all_books_page():
+    from bs4 import BeautifulSoup
+    d = {}
+    html_doc = str(open("all_books.html", "r").readlines())
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    books = soup.findAll("tr", {"class": "book"})
+    # Parse Course
+    for course_elm in (soup.find_all("span", {"id": "course-bookdisplay-coursename"})):
+        course = course_elm.text.split(",")[0]
+        d[course] = []
+        table = course_elm.find_next("table", {"class": "data"})
+        if table is not None:
+            # Parse Course Books
+            for book in table.find_all("tr", {"class": "book"}):
+                _class = book.get('class')
+                is_required = 'course-required' in _class
+                is_optional = 'course-optional' in _class
+                if is_required or is_optional:
+                    book_dict = {"is_required": is_required}
 
-#for isbn in soup.findAll("input", {"name": "isbn-1"}):
-#    print isbn
+                    # Parse Meta Data
+                    stevens_metadata = {}
+                    for x in ["book-title", "book-author", "isbn", "book-edition", "book-binding" ]:
+                        book_metadata_elm = book.find("span", {"class": x})
+                        if book_metadata_elm is not None:
+                            stevens_metadata[x] = book_metadata_elm.text
+                    book_dict["stevens-metadata"] = stevens_metadata
+
+                    # Parse Book Pricing
+                    stevens_pricing = {}
+                    for price in book.find_all("td", {"class": "price"}):
+                        price_label = price.find("label")
+                        book_type = price_label.get("for")[10:].split("_")[0]
+                        book_cost = price_label.text
+                        if book_cost != "N/A":
+                            stevens_pricing[book_type] = float(book_cost[1:])
+                    book_dict["stevens-metadata"] = stevens_metadata
+                    d[course].append(book_dict)
+    return d
+
+print json.dumps(parse_all_books_page(), sort_keys=True, indent=4, separators=(',', ': '))
 
