0f2e629eabd98bd88644b40d157a34a1d7a70ca5
Constantine Davantzis <c.davantzis@gmail.com>
2017-02-22 19:01:15 -0500

    Clean up stevens bookstore code

--- src/app/scrapping/stevens_bookstore.py
+++ src/app/scrapping/stevens_bookstore.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
-# coding=utf-8
 """ Module For Scraping Stevens Bookstore """
+# Standard Library Imports
 import time
 import pickle
-import requests
 import json
 
-from selenium import webdriver
+# 3rd Party Imports
+import requests
 
+# Project Imports
 from webdrivers import get_geckodriver_path
 
 __author__ = "Constantine Davantzis"
@@ -15,54 +16,66 @@ __status__ = "Development"
 
 
 def update_book_id_list(executable_path=get_geckodriver_path()):
+    """ Update stevens book id list pickle object with all book ids
+    :return: Weather or not the object was updated successfully
     """
+    try:
+        from selenium import webdriver
+    except ImportError:
+        return False, "Selenium must be installed on server"
 
-    :return:
-    """
-    r = []
+    try:
+        r = []
+        driver = webdriver.Firefox(executable_path=executable_path)
 
-    driver = webdriver.Firefox(executable_path=executable_path)
+        def get_options_for(_id):
+            select_box = driver.find_element_by_id(_id)
+            return select_box.find_elements_by_tag_name("option")[1:]
 
-    def get_options_for(_id):
-        select_box = driver.find_element_by_id(_id)
-        return select_box.find_elements_by_tag_name("option")[1:]
-
-    driver.get("http://www.stevenscampusstore.com/buy_textbooks.asp")
-    select_term = driver.find_element_by_id("fTerm")
-    select_term.find_elements_by_tag_name("option")[1].click()
-    time.sleep(.5)
-    for dept in get_options_for("fDept"):
-        dept.click()
+        driver.get("http://www.stevenscampusstore.com/buy_textbooks.asp")
+        select_term = driver.find_element_by_id("fTerm")
+        select_term.find_elements_by_tag_name("option")[1].click()
         time.sleep(.5)
-        for course in get_options_for("fCourse"):
-            course.click()
+        for dept in get_options_for("fDept"):
+            dept.click()
             time.sleep(.5)
-            for section in get_options_for("fSection"):
-                r.append(section.get_attribute("value"))
+            for course in get_options_for("fCourse"):
+                course.click()
+                time.sleep(.5)
+                for section in get_options_for("fSection"):
+                    r.append(section.get_attribute("value"))
 
-    driver.close()
-    driver.quit()
+        driver.close()
+        driver.quit()
 
-    with open('book_id_list.p', 'wb') as f:
-        pickle.dump(r, f)
+        with open('data/stevens_bookstore_ids.p', 'wb') as f:
+            pickle.dump(r, f)
 
-    return True, "book_id_list.p updated successfully"
+        return True, "data/stevens_bookstore_ids.p updated successfully"
+    except Exception as e:
+        return False, e.message
 
 
 def get_book_id_list():
-    return pickle.load(open('book_id_list.p', 'rb'))
+    """ Load the list of all Stevens book IDs from pickle object
+    :return: List of Steevns Book Ids
+    """
+    try:
+        return pickle.load(open('data/stevens_bookstore_ids.p', 'rb'))
+    except:
+        return []
 
 
-def save_all_books_page():
-    r = requests.post('http://www.stevenscampusstore.com/textbook_express.asp?mode=2&step=2', data={'sectionIds': get_book_id_list()})
-    with open("all_books.html", "w") as text_file:
-        text_file.write(r.content)
+def request_book_info(*ids):
+    """ Make a request to stevens campus store for the book info of the ids provided """
+    r = requests.post('http://www.stevenscampusstore.com/textbook_express.asp?mode=2&step=2', data={'sectionIds': ids})
+    return r.content
 
 
-def parse_all_books_page():
+def parse_book_info(html_doc):
+    """ Parse the book info """
     from bs4 import BeautifulSoup
     d = {}
-    html_doc = str(open("all_books.html", "r").readlines())
     soup = BeautifulSoup(html_doc, 'html.parser')
     books = soup.findAll("tr", {"class": "book"})
     # Parse Course
@@ -84,7 +97,7 @@ def parse_all_books_page():
                     for x in ["book-title", "book-author", "isbn", "book-edition", "book-binding" ]:
                         book_metadata_elm = book.find("span", {"class": x})
                         if book_metadata_elm is not None:
-                            stevens_metadata[x] = book_metadata_elm.text
+                            stevens_metadata[x] = book_metadata_elm.text.replace(u"\u00a0", " ")
                     book_dict["stevens-metadata"] = stevens_metadata
 
                     # Parse Book Pricing
@@ -99,5 +112,13 @@ def parse_all_books_page():
                     d[course].append(book_dict)
     return d
 
-print json.dumps(parse_all_books_page(), sort_keys=True, indent=4, separators=(',', ': '))
 
+def update_book_info():
+    """ Update stevens book info file """
+    data = parse_book_info(request_book_info(get_book_id_list()))
+    with open('data/stevens_bookstore_info.json', 'w') as f:
+        json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+
+if __name__ == "__main__":
+    update_book_info()
