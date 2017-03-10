de9a72d96228b2e4cf179ee37a68a6571f9c326a
Constantine Davantzis <c.davantzis@gmail.com>
2017-03-10 01:11:58 -0500

    clean up

--- src/app/courses/views.py
+++ src/app/courses/views.py
@@ -1,18 +1,22 @@
+# coding=utf-8
+""" '/courses' Views """
 from flask import Blueprint, render_template, abort
-
 import tools
-blueprint = Blueprint('courses', __name__)
+
+blueprint = Blueprint('courses', __name__, url_prefix="/courses")
 
 
-@blueprint.route('/courses/')
+@blueprint.route('/')
 def display_courses_by_departments():
     """ Display Courses By Departments """
+
     # TODO: MongoDB Exceptions
     departments = tools.get_courses_by_departments()
+
     return render_template('courses/by_department.html', departments=departments)
 
 
-@blueprint.route('/courses/<letter>-<number>')
+@blueprint.route('/<letter>-<number>')
 def display_course(letter, number):
     """ Display Course Page
     :param letter: Course Letters
