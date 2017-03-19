49ea01bda5c0f5fef43913f0c56c9c409785e8e4
enptfb55 <enptfb55@users.noreply.github.com>
2017-03-19 11:09:42 -0400

    fixed search after rebase with develop

--- src/app/static/js/search.js
+++ src/app/static/js/search.js
@@ -2,32 +2,17 @@
 function frontPageSearch() {
     // Declare variables
     var input, filter, list, div, a, i;
-    input = document.getElementById('search-bar');
+    input = document.getElementById('jumbo-search');
     filter = input.value.toUpperCase();
     list = document.getElementById("book-list");
-    //div = list.getElementsByTagName('div');
-    div = $("#book-list").children();
-
+    books = list.getElementsByTagName('div');
 
     // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < div.length; i++) {
-        // search header links
-        a = div[i].getElementsByTagName("a")[0];
-        var books = div[i].getElementsByTagName("div");
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            div[i].style.display = "";
+    for (i = 0; i < books.length; i++) {
+        if (books[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            books[i].style.display = "";
         } else {
-            div[i].style.display = "none";
-        }
-
-        // search books
-        for (var j = 0; j < books.length; j++) {
-            if (books[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-                div[i].style.display = "";
-                books[j].style.display = "";
-            } else {
-                books[j].style.display = "none";
-            }
+            books[i].style.display = "none";
         }
     }
 }
