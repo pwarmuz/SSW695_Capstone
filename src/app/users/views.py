e39fc41e966275be7b987affb7464e76e7f7ecb4
Sal Amato <enptfb55@gmail.com>
2017-04-12 19:06:26 -0400

    Fixed sign-up functionality

--- src/app/users/views.py
+++ src/app/users/views.py
@@ -3,30 +3,34 @@ from flask import Blueprint, request, redirect, render_template, url_for, flash,
 from flask_login import login_user, logout_user
 from manager import User
 from app import mongo_client
+import tools
 
 blueprint = Blueprint('Users', __name__)
 
 
-#@blueprint.route('/signup', methods=['POST'])
-#def signup():
-#    name = request.form.get('name')
-#    email = request.form.get('email')
-#    password = request.form.get('password')
-#
-#    r = mongo_client.ssw695.users.find_one({"email": email},
-#                                           {'_id': False})
-#
-#    if r is not None:
-#        return 'already signed up!'
-#
-#    user = {'name': name,
-#            'email': email,
-#            'password': password}#
-#
-#    mongo_client.ssw695.users.insert_one(user)
-#
-#   session['username'] = name
-#    return redirect(url_for('home'))
+@blueprint.route('/signup', methods=['POST'])
+def signup():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    r = mongo_client.ssw695.users.find_one({"email": email},
+                                           {'_id': False})
+
+    if r is not None:
+        return 'already signed up!'
+
+
+    print 'adding email: {0} password: {1}'.format(email, password)
+
+    ret = tools.create_new_user(email=email, 
+                                password=password)
+
+    if ret.get('success') == True:
+        user = User(email)
+        login_user(user)
+
+    return redirect(url_for('home'))
 
 
 @blueprint.route('/login', methods=['POST'])
