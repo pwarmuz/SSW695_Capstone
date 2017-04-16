c73d69555e0193d86104454875eb2ec0a69d820d
Constantine Davantzis <c.davantzis@gmail.com>
2017-04-16 02:27:50 -0400

    clean up navbar

--- src/app/books/views.py
+++ src/app/books/views.py
@@ -9,12 +9,12 @@ blueprint = Blueprint('books', __name__, url_prefix="/books")
 @blueprint.route('/')
 def display_all_books():
     """ Display All Books """
-    pass
+    return render_template('books/all.html')
 
 
 @blueprint.route('/<isbn>')
 def display_book(isbn):
-    """ Display a specific book based on the ISBN number
+    """ Display a specific books based on the ISBN number
     :param isbn: isbn Number (10-digit / 13-digit)
     """
 
@@ -26,4 +26,4 @@ def display_book(isbn):
 
     seller_list = tools.query_sales_listing(isbn)
     session['isbn_value'] = isbn
-    return render_template('book/book_isbn.html', book=book, seller_list=seller_list)
+    return render_template('books/book_isbn.html', book=book, seller_list=seller_list)
